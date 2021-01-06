d424e3dd3e8da463599c355f61867f754e76bbc4
Tejaswi Samrat Dasari <sdasari625@gmail.com>
2021-01-07 00:02:14 +0530

    no merge conflict

--- b1.py
+++ b1.py
@@ -1,13 +1,7 @@
 print("This is b1 py")
 
 def funcb1c(a):
-<<<<<<< HEAD
-	print("in b1c branch")
-	print("change to be shown in master")
-	return a
-=======
 	a = 2
 	b = 4
 	print("new statement")
 	return a
->>>>>>> b1c
